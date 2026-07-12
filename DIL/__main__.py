5cae3bf85e2cb5ad55e86e072ce4d2706499a33b
sᴀɢᴀʀ ᴛɪᴡᴀʀɪ <112709751+stkeditz@users.noreply.github.com>
2026-07-13 05:08:08 +0530

    Update __main__.py

--- DIL/__main__.py
+++ DIL/__main__.py
@@ -1,44 +1,63 @@
-import asyncio, importlib
+import asyncio
+import importlib
 
-from pytgcalls import idle
-
-from . import logs, plugs, vars
+from . import logs, plugs
 from .plugins import ALL_PLUGINS
+
 from .modules.clients.clients import run_async_clients
 from .modules.clients.enums import run_async_enums
 from .modules.helpers.inline import run_async_inline
-#from .modules.utilities.tgcalls import run_async_calls
 
 
 async def main():
+
     await run_async_clients()
+
     for all_plugin in ALL_PLUGINS:
         imported_plugin = importlib.import_module(
             "DIL.plugins" + all_plugin
         )
-        if (hasattr
-            (
-                imported_plugin, "__NAME__"
-            ) and imported_plugin.__NAME__
+
+        if (
+            hasattr(imported_plugin, "__NAME__")
+            and imported_plugin.__NAME__
         ):
-            imported_plugin.__NAME__ = imported_plugin.__NAME__
+
             if (
-                hasattr(
-                    imported_plugin, "__MENU__"
-                ) and imported_plugin.__MENU__
+                hasattr(imported_plugin, "__MENU__")
+                and imported_plugin.__MENU__
             ):
-                plugs[imported_plugin.__NAME__.lower()
+
+                plugs[
+                    imported_plugin.__NAME__.lower()
                 ] = imported_plugin
+
+
     await run_async_enums()
-    logs.info(">> Sᴜᴄᴄᴇssғᴜʟʟʏ Iᴍᴘᴏʀᴛᴇᴅ Aʟʟ Pʟᴜɢɪɴs.")
+
+    logs.info(
+        ">> Successfully Imported All Plugins."
+    )
+
+
     await run_async_inline()
-    logs.info("SᴜᴄᴄᴇssFᴜʟʟʏ DᴇᴘLᴏʏᴇᴅ !!")
-    await run_async_calls()
-    logs.info("Dᴏ Vɪsɪᴛ - @alonegroup121")
-    await idle()
+
+    logs.info(
+        "Successfully Deployed!"
+    )
+
+
+    await asyncio.Event().wait()
+
 
 
 if __name__ == "__main__":
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    print("Userbot Stopped !\nGoodBye ...")
+
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        print(
+            "Userbot Stopped!\nGoodBye..."
+        )
