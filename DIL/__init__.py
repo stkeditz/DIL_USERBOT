2800f03887a0738303898762275999261a357b7d
sᴀɢᴀʀ ᴛɪᴡᴀʀɪ <112709751+stkeditz@users.noreply.github.com>
2026-07-13 05:07:23 +0530

    Update __init__.py

--- DIL/__init__.py
+++ DIL/__init__.py
@@ -1,19 +1,19 @@
-
 __version__ = "v2.1.0"
 
-
-
 from .modules.clients.clients import (
-    app, bot, call
+    app,
+    bot
 )
+
 app = app
 bot = bot
-call = call
 
 
 from .modules.helpers.filters import (
-    commandx, commandz
+    commandx,
+    commandz
 )
+
 cdx = commandx
 cdz = commandz
 
@@ -21,24 +21,29 @@ cdz = commandz
 from .modules.helpers.events import (
     edit_or_reply
 )
+
 eor = edit_or_reply
 
 
 from .console import LOGGER
+
 logs = LOGGER
 
 
 from .console import PLUGINS
+
 plugs = PLUGINS
 
 
 from . import console as config
+
 vars = config
 
 
 from .modules.helpers.wrapper import (
-    super_user_only, sudo_users_only
+    super_user_only,
+    sudo_users_only
 )
+
 super_user_only = super_user_only
 sudo_users_only = sudo_users_only
-
